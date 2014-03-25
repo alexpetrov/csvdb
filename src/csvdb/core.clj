f7be0fc6c2641b2ca4c189fe1ae6e41789eb3553
alexpetrov <alexpetrov.rb@gmail.com>
2014-03-25 23:46:03 +0400

    Fixed bug in join* implementation with nested reduce. Use into instead of conj/first to add all records instead of one.

--- src/csvdb/core.clj
+++ src/csvdb/core.clj
@@ -90,6 +90,8 @@
      (sort-by column data)
      data))
 
+;; (join* student :id student-subject :student_id )
+;; (join* (join* student :id student-subject :student_id ) :subject_id subject :id)
 ;; (join* (join* student-subject :student_id student :id) :subject_id subject :id)
 ;; => [{:subject "Math", :subject_id 1, :surname "Ivanov", :year 1998, :student_id 1, :id 1}
 ;;     {:subject "Math", :subject_id 1, :surname "Petrov", :year 1997, :student_id 2, :id 2}
@@ -104,27 +106,20 @@
   ;; 3. For each element of data1 (lets call it element1) find all elements of data2 (lets call each as element2) where column1 = column2.
   ;; 4. Use function 'merge' and merge element1 with each element2.
   ;; 5. Collect merged elements.
-  ;; (reduce
-  ;;   (fn [acc elem]
-  ;;     (conj
-  ;;       acc
-  ;;         ;; (reduce
-  ;;         ;;   #(conj %1 (merge %2 elem))
-  ;;         ;;   []
-  ;;         ;;   (filter #(= (column1 elem) (column2 %)) data2))
-
-  ;;       (first
-  ;;         (reduce
-  ;;           #(conj %1 (merge %2 elem))
-  ;;           []
-  ;;           (filter #(= (column1 elem) (column2 %)) data2))
-  ;;         )
-  ;;       ))
-  ;;   []
-  ;;   data1)
-  (for [element1 data1
-        element2 data2
-        :when (= (column1 element1) (column2 element2) )] (merge element1 element2))
+  (reduce
+    (fn [acc elem]
+      (into
+        acc
+        (reduce
+          #(conj %1 (merge %2 elem))
+          []
+          (filter #(= (column1 elem) (column2 %)) data2))
+        ))
+    []
+    data1)
+  ;; (for [element1 data1
+  ;;       element2 data2
+  ;;       :when (= (column1 element1) (column2 element2) )] (merge element1 element2))
 )
 
 ;; (= {:a 1 :b 2} {:b 2 :a 1})
